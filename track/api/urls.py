49f4340ea83704c34b91bc933acb7cb22057a91a
Dev-Badr <36001332+Dev-Badr@users.noreply.github.com>
2019-04-17 15:18:48 +0200

    Add URLs for Serializers Models

--- track/api/urls.py
+++ track/api/urls.py
@@ -1,5 +1,15 @@
 from django.urls import path
-from track.api.views import TrackList, TrackDetail
+
+from track.api.views import (
+	TrackList, TrackDetail,
+	CourseList, CourseDetail,
+	UnitList, UnitDetail,
+	LectureList, LectureDetail,
+	CourseMemberList, CourseMemberDetail,
+	CategoryList, CategoryDetail,
+	ArticleList, ArticleDetail,
+	PracticeList, PracticeDetail
+	)
 
 # from rest_framework import routers
 # from track.api.views import TrackViewSet
@@ -10,8 +20,29 @@ from track.api.views import TrackList, TrackDetail
 app_name = 'api-track'
 
 urlpatterns = [
-	path('api-tracks/', TrackList.as_view(), name='track-list'),
-	path('api-tracks/<pk>/', TrackDetail.as_view(), name='track-detail'),
+	path('tracks/', TrackList.as_view()),
+	path('tracks/<int:pk>/', TrackDetail.as_view()),
+
+	path('courses/', CourseList.as_view()),
+	path('courses/<int:pk>/', CourseDetail.as_view()),
+
+	path('units/', UnitList.as_view()),
+	path('units/<int:pk>/', UnitDetail.as_view()),
+
+	path('lectures/', LectureList.as_view()),
+	path('lectures/<int:pk>/', LectureDetail.as_view()),
+
+	path('course-members/', CourseMemberList.as_view()),
+	path('course-members/<int:pk>/', CourseMemberDetail.as_view()),
+
+	path('categories/', CategoryList.as_view()),
+	path('categories/<int:pk>/', CategoryDetail.as_view()),
+
+	path('articles/', ArticleList.as_view()),
+	path('articles/<int:pk>/', ArticleDetail.as_view()),
+
+	path('practices/', PracticeList.as_view()),
+	path('practices/<int:pk>/', PracticeDetail.as_view()),
 ] 
 
 # urlpatterns += router.urls
