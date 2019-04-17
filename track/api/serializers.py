550e7ceef24860cc4c16b6851d77fbb8d99ed558
Dev-Badr <36001332+Dev-Badr@users.noreply.github.com>
2019-04-17 15:13:42 +0200

    Update serializers.py

--- track/api/serializers.py
+++ track/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from track.models import Track, Course
+from track.models import Track, Course, Unit, Lecture, CourseMember, \
+						 Category, Article, Practice
 
 class TrackSerializer(serializers.ModelSerializer):
 	class Meta:
@@ -9,4 +10,35 @@ class TrackSerializer(serializers.ModelSerializer):
 class CourseSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Course
+		fields = '__all__'
+
+class UnitSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Unit
+		fields = '__all__'
+
+class LectureSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Lecture
+		fields = '__all__'
+
+class CourseMemberSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = CourseMember
+		fields = '__all__'
+
+class CategorySerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Category
+		fields = '__all__'
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Article
+		fields = '__all__'
+
+class PracticeSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Practice
 		fields = '__all__'
