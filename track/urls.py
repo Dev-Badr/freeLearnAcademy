2539d088167c41376344f4e51f79e3b160883744
badr kamel <badrk.shokr@gmail.com>
2019-04-19 01:33:06 +0200

    Update URLs

--- track/urls.py
+++ track/urls.py
@@ -7,69 +7,71 @@ app_name = 'track'
 
 urlpatterns = [
     
-    # SEARCH
-    # v.4x we must search in also specific category and tag
-    # path('courses/search', search_track, name='course_search'),
-    # path('users/search', search_track, name='user_search'),
-
-	# tracks
+    # v.4x We must search in a category and a specific tag as well
+    
+	# All tracks
     path('tracks/', TrackView.as_view(), name='track-list'),
     path('tracks/<slug:slug>/', TrackDetailView.as_view(), name='track-detail'),
 
-    # courses
+    # Courses
     path('courses/', CourseView.as_view(), name='course-list'),
-    #ajax search
+
+    # Ajax search
     path('courses/search', course_search, name='course_search'),
 
-    # my courses
+    # My courses
     path('account/dashboard', DashboardView.as_view(), name='dashboard'),
 
-    # course detail
+    # Course detail
     path('courses/<slug:slug>/', 
             CourseDetailView.as_view(), name='course-detail'),
 
-    #join or leave track
+    # Join the track
     path('course/<slug:slug>/join', JoinCourse.as_view(), name='join'),
+
+    # Leave the track
     path('course/<slug:slug>/leave', LeaveCourse.as_view(), name='leave'),
 
     ############################ articles #############################
 
-    # All
+    # Whole articles
     path('articles/', ModelListView.as_view(model=Article), name='article-list'),
     
-    #ajax search
+    # ajax search
     path('articles/search', article_search, name='article_search'),
 
-    # articles by track_slug
+    # articles from a particular track
     path('<slug:track_slug>/articles', login_required(
             ModelListView.as_view(model=Article)), name='article_list_by_track'),
 
-    # articles by category_slug
+    # articles from a particular category
     path('articles/category/<slug:category_slug>', login_required(
             ModelListView.as_view(model=Article)),
             name='article_list_by_category'),
 
-    # articles by tag_slug
+    # articles from a particular tag
     path('articles/tag/<slug:tag_slug>', login_required(
             ModelListView.as_view(model=Article)), name='article_list_by_tag'),
     
-    # Detail
+    # track detail
     path('article/<int:id>/<slug:slug>', 
             ArticleDetailView.as_view(model=Article), name='article-detail'),
 
     ########################### practices #############################
 
-    # practices
+    # Whole articles
     path('practices/', ModelListView.as_view(
                         model=Practice), name='practice-list'),
 
+    # Practices from a particular track
     path('<slug:track_slug>/practices', login_required(
             ModelListView.as_view(model=Practice)), name='practice_list_by_track'),
     
+    # Practices from a particular category
     path('practices/category/<slug:category_slug>', login_required(
             ModelListView.as_view(model=Practice)), name='practice_list_by_category'),
 
-    # practice detail
+    # Practice detail
     path('practices/<slug:slug>', 
             PracticeDetailView.as_view(), name='practice-detail'),
 
@@ -84,21 +86,11 @@ urlpatterns = [
      ##################################################################
 
 
-    # those urls must be in the buttom
-    # units    
+    # Must be in the buttom. So that the URLs don't conflict
+    # Course modules    
     path('<slug:course_slug>/<slug:slug>',
             UnitDetailView.as_view(), name='unit-detail'),
     # lectures
     path('<slug:course_slug>/<slug:slug>/<slug:lecture_slug>', 
             UnitDetailView.as_view(), name='lecture-detail'),
-
-    # path('<slug:course_slug>/<slug:slug>', RedirectView.as_view(
-    #         url=reverse_lazy('track:lecture-detail')), name='unit-detail' ),
-
 ]
-
-
-# Create fake data
-    # path('create_users/<int:number>', create_users, name='create_users'),
-    # path('create_videos/<int:number>', create_videos, name='create_items'),
-    # path('create_category/<int:number>', create_category, name='create_category'),
