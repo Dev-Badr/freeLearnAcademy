6d95f042d1075f025c70ae0ec72a94b73463ffcc
badr kamel <badrk.shokr@gmail.com>
2019-04-25 23:59:32 +0200

    The login API

---
+++ accounts/api/views.py
@@ -0,0 +1,16 @@
+from rest_framework.views import APIView
+from rest_framework import status
+from django.contrib.auth import authenticate
+
+class LoginView(APIView):
+	permission_classes = ()
+
+	def post(self, request,):
+		username = request.data.get("username")
+		password = request.data.get("password")
+		user = authenticate(username=username, password=password)
+		if user:
+			return Response({"token": user.auth_token.key})
+		else:
+			return Response({"error": "Wrong Credentials"},
+							status=status.HTTP_400_BAD_REQUEST)
